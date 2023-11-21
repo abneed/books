8aaa5a1300624b16baa3fd2eb0be9e4a8c771a92
Abneed <abneed.rodriguez@gmail.com>
2023-11-21 17:25:54 -0600

    Backup all changes from the course

---
+++ src/components/BookList.jsx
@@ -0,0 +1,18 @@
+import { useContext } from "react";
+import BooksContext  from "../context/books";
+import BookShow from "./BookShow";
+
+function BookList({ books, onEdit, onDelete }) {
+	const value = useContext(BooksContext);
+
+	const renderedBooks = books.map((book) => {
+		return <BookShow onEdit={onEdit} onDelete={onDelete} key={book.id} book={book} />;
+	});
+
+	return <div className="book-list">
+		{value}
+		{renderedBooks}
+	</div>;
+}
+
+export default BookList;
